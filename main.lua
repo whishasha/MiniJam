fe86ae77b32cd888c16a37ca2369e48ca8f947eb
whishasha <wilsonma12746@gmail.com>
2025-11-22 21:17:47 +1100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 _G.love = require("love")
 local sti = require("sti")
+
+
 function love.load()
     Map = sti("map.lua", {'box2d'})
     World = love.physics.newWorld(0, 0)
-    Map:box2d_init(world)
+    Map:box2d_init(World)
     Map.layers.collision.visible = false
 
 end
