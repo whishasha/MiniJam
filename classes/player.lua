d491552d678ee8a4834c786a5de5b31ac4423905
Kevin Ma <gavelhj@Kevins-MacBook-Air.local>
2025-11-22 21:38:42 +1100

    rectangle update

--- classes/player.lua
+++ classes/player.lua
@@ -1,21 +1,23 @@
+_G.love = require("love")
 Player = {}
 Player.__index = Player
 
-function Player.new(self, keybinds, x, y, width, height, friction, acceleration, maxspeed)
+function Player.new(self, keybinds, x, y, width, height, friction, acceleration, maxspeed, gravity)
     local self = self or {}
     
     self.xVel = 0
-    self.yVel = 10
+    self.yVel = 100
 
     self.x = x or 200
-    self.y = y or 0
+    self.y = y or 200
 
-    self.width = width or 10
-    self.height = height or 10
+    self.width = width or 100
+    self.height = height or 100
 
     self.friction = friction or 3500
     self.acceleration = acceleration or 4000
     self.maxSpeed = maxspeed or 100
+    self.gravity = gravity or 1500
 
     self.physics = {}
     self.physics.body = love.physics.newBody(World, self.x, self.y, "dynamic")
@@ -28,12 +30,13 @@ function Player.new(self, keybinds, x, y, width, height, friction, acceleration,
     return setmetatable(self, Player)
 end
 
-local function syncPhysics(self)
+function Player:syncPhysics()
     self.x, self.y = self.physics.body:getPosition()
+    self.physics.body:setLinearVelocity(self.xVel, self.yVel)
 end
 
 function Player:update(dt)
-    syncPhysics(self)
+    self:syncPhysics()
 end
 
 function Player:draw()
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 _G.love = require("love")
 local sti = require("sti")
 
+require("classes.player")
+
 
 function love.load()
     Map = sti("map.lua", {'box2d'})
@@ -8,16 +10,17 @@ function love.load()
     Map:box2d_init(World)
     Map.layers.collision.visible = false
 
-    local plr = Player.new()
+    _G.plr = Player.new()
 
 end
 
 function love.update(dt)
     -- empty
     World:update(dt)
+    plr:update(dt)
 end
 
 function love.draw()
     Map:draw(0, 0, 1, 1)
+    plr:draw()
 end
-
